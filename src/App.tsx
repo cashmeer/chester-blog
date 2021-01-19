203e1536abc87da3973aa1de827450dc6abc9487
Jongsu Lee <jongsu.lee@bigin.io>
2021-01-19 19:22:13 +0900

    4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://cdn141.picsart.com/317732747536211.png"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
